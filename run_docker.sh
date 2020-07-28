c29a8175f823093a4338f3d670a704f0f2aa85cc
Liu,Ruix <ruix.liu@intel.com>
2020-07-28 08:50:57 +0800

    again 6

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
 
-DOCKER_NAME=VCFO-UNDEBUG-V1
-if [ $1 ]
-then
-  DOCKER_NAME=$1
-fi
-docker run --name=${DOCKER_NAME} -t -d 10.67.111.136/pub/vcfo_undebug:v1-0723
-echo "Success to build a container, and name is ${DOCKER_NAME}"
